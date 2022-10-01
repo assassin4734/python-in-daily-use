38f6556f3413beffb3dd5e4966cdd12aa033a549
assassin4734 <490645473@qq.com>
2022-10-01 12:27:14 +0800

    循环中的x位置写错了，已修改

--- 5. python handle tecplot/2. tecplot批量导出线上数据.py
+++ 5. python handle tecplot/2. tecplot批量导出线上数据.py
@@ -8,6 +8,7 @@ tp.session.connect(port=7600)
 
 eq_folder = ["eq=0.55", "eq=0.65", "eq=0.75", "eq=0.85", "eq=0.95"]
 scale_factor = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+line_position = ['0.333333', '0.666667', '1.33333', '2']
 for folders in eq_folder:
     # 地址格式举例：F:\\PhD\\1 nozzle\\eq\\postprocessing\\eq=0.55
     dir_colletion = 'F:\\PhD\\1 nozzle\\eq\\postprocessing\\' + folders
@@ -17,21 +18,22 @@ for folders in eq_folder:
         # 地址格式举例：F:\\PhD\\1 nozzle\\eq\\postprocessing\\eq=0.55\\40.5-1
         dir_position = dir_colletion + '\\40.5-' + str_factors
         # 定义导出地址
-        pos = 1
+        pos = 0
         name_list = []
-        while pos < 5:
-            locals()['position' + str(pos) + '_com'] = "'XSTART = 0.333333 YSTART = -1.5 ZSTART = 0 XEND = 0.333333 YEND = 1.5 ZEND = 0 NUMPTS = 200 EXTRACTTHROUGHVOLUME = F EXTRACTTOFILE = T EXTRACTFILENAME = " + "\\'" + dir_position  + "\\POSITION" + str(pos) + ".txt\\' '"
-            name_list.append(locals()['position' + str(pos) + '_com'])
+        while pos < 4:
+            x_start = line_position[pos]
             pos += 1
+            locals()['position' + str(pos) + '_com'] = "'XSTART = " + x_start + " YSTART = -1.5 ZSTART = 0 XEND = " + x_start + " YEND = 1.5 ZEND = 0 NUMPTS = 200 EXTRACTTHROUGHVOLUME = F EXTRACTTOFILE = T EXTRACTFILENAME = " + "\\'" + dir_position  + "\\POSITION" + str(pos) + ".txt\\' '"
+            name_list.append(locals()['position' + str(pos) + '_com'])
         file_dir = dir_position + "\\export.txt"
         file_com = open(file_dir,'w')
         # file_com.write("")
         file_com.write("#!MC 1410\n$!RedrawAll\n$!ExtendedCommand\n  CommandProcessorID = 'Extract Precise Line'\n  Command = " + name_list[0] + "\n$!ExtendedCommand\n  CommandProcessorID = 'Extract Precise Line'\n  Command = " + name_list[1] + "\n$!ExtendedCommand\n  CommandProcessorID = 'Extract Precise Line'\n  Command = "  + name_list[2] + "\n$!ExtendedCommand\n  CommandProcessorID = 'Extract Precise Line'\n  Command = " + name_list[3])
         file_com.close()
         portion = os.path.splitext(file_dir)
-        newname = portion[0] + "2.mcr"
+        newname = portion[0] + ".mcr"
         os.rename(file_dir, newname)
-        file_dir2 = dir_position + "\\export2.mcr"
+        file_dir2 = dir_position + "\\export.mcr"
         tp.macro.execute_file(file_dir2)
         print(dir_position + ' has done')
 input("all done, press enter to exit")
