635074373940d2a1d42bd3214c2d875e5cace7d1
assassin4734 <490645473@qq.com>
2022-10-01 15:07:15 +0800

    加上判断，已经处理过的就不再处理

--- 0. python operate os/5. 图片裁剪.py
+++ 0. python operate os/5. 图片裁剪.py
@@ -1,7 +1,9 @@
 # _*_ encoding utf-8 _*_
 
 import cv2
- 
+import re
+import os
+
 
 eq_folder = ["eq=0.55", "eq=0.65", "eq=0.75", "eq=0.85", "eq=0.95"]
 scale_factor = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
@@ -16,18 +18,27 @@ for folders in eq_folder:
         # 地址格式举例：F:\\PhD\\1 nozzle\\eq\\postprocessing\\eq=0.55\\40.5-1
         dir_position = dir_colletion + '\\40.5-' + str_factors
         print(dir_position + ' is on going')
-        for phs in range(len(photos)):
-            phs_str = photos[phs]
-            photo_position = dir_position + '\\' + phs_str
-            print(photo_position)
-            image_cut = cv2.imread(photo_position)
-            cv2.imshow("image", image_cut)
-            size = image_cut.shape[0:3]
-            print(size)
-            if phs == 0:
-                image_cut = image_cut[0:390, 25:730]
-            else:
-                image_cut = image_cut[0:390, 25:355]
-            cv2.imwrite(photo_position.strip(".tiff") + "-cut.tiff", image_cut)
-            cv2.destroyAllWindows()
+        files = os.listdir(dir_position)
+        tiffs = []
+        for file in files:
+            if re.search("-cut.tiff", file, re.I):
+                tiffs.append(file)
+                break
+        if tiffs == []:
+            for phs in range(len(photos)):
+                phs_str = photos[phs]
+                photo_position = dir_position + '\\' + phs_str
+                print(photo_position)
+                image_cut = cv2.imread(photo_position)
+                cv2.imshow("image", image_cut)
+                size = image_cut.shape[0:3]
+                print(size)
+                if phs == 0:
+                    image_cut = image_cut[0:390, 25:730]
+                else:
+                    image_cut = image_cut[0:390, 25:355]
+                cv2.imwrite(photo_position.strip(".tiff") + "-cut.tiff", image_cut)
+                cv2.destroyAllWindows()
+        else:
+            print("all photos had been exported")
 input("all done")
