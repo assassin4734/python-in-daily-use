1ee866ff69398691be8345a20c86ba9182a790bb
assassin4734 <490645473@qq.com>
2022-10-01 10:29:49 +0800

    改成了循环的写法，更简洁了

--- 5. python handle tecplot/0. photo export.py
+++ 5. python handle tecplot/0. photo export.py
@@ -6,56 +6,34 @@ from tecplot.constant import *
 
 
 tp.session.connect(port=7600)
-
-file_folder = [28.5, 35.5, 40.5, 45.5, 52.5]
+# 定义目录
+eq_folder = ["eq=0.55", "eq=0.65", "eq=0.75", "eq=0.85", "eq=0.95"]
 scale_factor = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-
-for folders in file_folder:
-  for factors in scale_factor:
-
-    str_folders = str(folders)
-    str_factors = str(factors)
-    
-    dir_import_velocity = 'F:\\PhD\\1 nozzle\\different swirl number\\postprocessing\\z-' + str_folders + '\\' + str_folders + '-' + str_factors + '\\z-28.5-1-velocity.lay'
-    dir_import_flame = 'F:\\PhD\\1 nozzle\\different swirl number\\postprocessing\\z-' + str_folders + '\\' + str_folders + '-' + str_factors + '\\z-28.5-1-flame.lay'
-    dir_import_ch = 'F:\\PhD\\1 nozzle\\different swirl number\\postprocessing\\z-' + str_folders + '\\' + str_folders + '-' + str_factors + '\\z-28.5-1-ch+.lay'
-    dir_export_velocity = 'F:\\PhD\\1 nozzle\\different swirl number\\postprocessing\\z-' + str_folders + '\\' + str_folders + '-' + str_factors + '\\velocity.tiff'
-    dir_export_flame = 'F:\\PhD\\1 nozzle\\different swirl number\\postprocessing\\z-' + str_folders + '\\' + str_folders + '-' + str_factors + '\\flame.tiff'
-    dir_export_ch = 'F:\\PhD\\1 nozzle\\different swirl number\\postprocessing\\z-' + str_folders + '\\' + str_folders + '-' + str_factors + '\\ch+.tiff'
-    
-    print(str_folders + str_factors + 'is on going')
-    
-    tp.macro.execute_command('$!RedrawAll')
-    tp.load_layout(dir_import_velocity)
-    tp.export.save_tiff(dir_export_velocity,
-        width=730,
-        region=ExportRegion.AllFrames,
-        supersample=1,
-        convert_to_256_colors=False,
-        gray_scale_depth=None,
-        byte_order=TIFFByteOrder.Intel)
-    print('velocity photo exported')
-    tp.macro.execute_command('$!RedrawAll')
-    tp.load_layout(dir_import_flame)
-    tp.export.save_tiff(dir_export_flame,
-        width=730,
-        region=ExportRegion.AllFrames,
-        supersample=1,
-        convert_to_256_colors=False,
-        gray_scale_depth=None,
-        byte_order=TIFFByteOrder.Intel)
-    print('flame photo exported')
-    tp.macro.execute_command('$!RedrawAll')
-    tp.load_layout(dir_import_ch)
-    tp.export.save_tiff(dir_export_ch,
-        width=730,
-        region=ExportRegion.AllFrames,
-        supersample=1,
-        convert_to_256_colors=False,
-        gray_scale_depth=None,
-        byte_order=TIFFByteOrder.Intel)
-    print('ch photo exported')
-
+layouts = ['-velocity.lay', '-flame.lay', '-ch+.lay']
+for folders in eq_folder:
+    # 地址格式举例：F:\\PhD\\1 nozzle\\eq\\postprocessing\\eq=0.55
+    dir_colletion = 'F:\\PhD\\1 nozzle\\eq\\postprocessing\\' + folders
+    print(dir_colletion + ' is on processing')
+    for factors in scale_factor:
+        str_factors = str(factors)
+        # 地址格式举例：F:\\PhD\\1 nozzle\\eq\\postprocessing\\eq=0.55\\40.5-1
+        dir_positiion = dir_colletion + '\\40.5-' + str_factors
+        for lays in layouts:
+            # 地址格式举例：F:\\PhD\\1 nozzle\\eq\\postprocessing\\eq=0.55\\40.5-1\\z-28.5-1-velocity.lay
+            dir_final = dir_positiion + '\\z-28.5-1' + lays
+            print(dir_final + ' is on going')
+            photo_name = lays.strip('z-28.5-1-').replace('.lay','')
+            dir_save = dir_positiion + '\\' + photo_name + '.tiff'
+            tp.macro.execute_command('$!RedrawAll')
+            tp.load_layout(dir_final)
+            tp.export.save_tiff(dir_save,
+                width=730,
+                region=ExportRegion.AllFrames,
+                supersample=1,
+                convert_to_256_colors=False,
+                gray_scale_depth=None,
+                byte_order=TIFFByteOrder.Intel)
+            print(photo_name + ' photo exported')
 input("all done")
 
 
