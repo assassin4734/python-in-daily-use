6c6e0fda6f6bea2f319432c949b89950ddb2737e
assassin4734 <490645473@qq.com>
2022-10-03 23:26:56 +0800

    根据五喷嘴目录结构改

--- 0. python operate os/2. 批量创建文件夹.py
+++ 0. python operate os/2. 批量创建文件夹.py
@@ -3,19 +3,22 @@
 import os
 
 
-
 # 定义目录变量
-file_folder = ["eq=0.55", "eq=0.65", "eq=0.75", "eq=0.85", "eq=0.95"]
-scale_factor = [1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]
+distance_folder = ['48', '60', '72']
+nozzles_folder = ['5nozzle', '3nozzle', '1nozzle']
+scale_factor = [1, 0.9, 0.8, 0.7, 0.6, 0.45, 0.4, 0.35, 0.3, 0.25]
 # 定义一级目录
-for folders in file_folder:
-    str_folders = str(folders)
-    dir_colletion = 'F:\\PhD\\1 nozzle\\eq\\postprocessing\\' + str_folders
-    print(dir_colletion + ' is on processing')
-# 定义二级目录
-    for num in range(len(scale_factor)):
-        factors = scale_factor[num]
-        str_factors = str(factors) 
-        dir_working = dir_colletion + '\\40.5-' + str_factors
-        print(dir_working + ' is on going')
-        os.makedirs(dir_working)
+for folders in nozzles_folder:
+    # E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\postprocessing\\5nozzle
+    dir_nozzles = 'E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\postprocessing\\' + folders
+    print(dir_nozzles + ' is on processing')
+    for distance in distance_folder:
+        # E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\postprocessing\\5nozzle\\48
+        dir_distance = dir_nozzles + '\\' + distance
+    # 定义二级目录
+        for num in range(len(scale_factor)):
+            str_factors = str(scale_factor[num]) 
+            # E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\postprocessing\\5nozzle\\48\\48-1
+            dir_scale = dir_nozzles + '\\' + distance + '-' + str_factors
+            print(dir_scale + ' is on going')
+            os.makedirs(dir_scale)
