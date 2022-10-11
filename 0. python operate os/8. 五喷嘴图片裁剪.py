1a8918784fd944ed739a9a886619b784e2bb4bb5
assassin4734 <490645473@qq.com>
2022-10-11 11:37:47 +0800

    更新

---
+++ 0. python operate os/8. 五喷嘴图片裁剪.py
@@ -0,0 +1,34 @@
+# _*_ encoding utf-8 _*_
+
+
+import cv2
+
+
+distance_folder = ['48', '60', '72']
+nozzles_folder = ['5nozzle', '3nozzle', '1nozzle']
+scale_factor = [1, 0.9, 0.8, 0.7, 0.6, 0.45, 0.4, 0.35, 0.3, 0.25]
+quantities = ['velocity.tiff', 'ch+.tiff', 'flame.tiff']
+for nozzles in nozzles_folder:
+    # E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\5nozzle
+    dir_nozzles = 'E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\postprocessing\\' + nozzles
+    print(dir_nozzles + ' is on processing')
+    for distance in distance_folder:
+        # E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\5nozzle\\48
+        dir_distance = dir_nozzles + '\\' + distance
+        for num in range(len(scale_factor)):
+            str_factors = str(scale_factor[num]) 
+            #  E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\5nozzle\\48\\48-1
+            dir_scale = dir_distance + '\\' + distance + '-' + str_factors
+            for quantity in quantities:
+                dir_phs = dir_scale + '\\' + quantity
+                print(dir_phs + ' is on going')
+                image_cut = cv2.imread(dir_phs)
+                size = image_cut.shape[0:3]
+                print(size)
+                if quantity == 'velocity.tiff':
+                    image_cut = image_cut[0:939, 65:1642]
+                else:
+                    image_cut = image_cut[0:939, 65:620]
+                cv2.imwrite(dir_phs.replace(".tiff", "-cut.tiff"), image_cut)
+                cv2.destroyAllWindows()
+input("all done")
---
+++ 5. python handle tecplot/2. 五喷嘴目录tecplot导出.py
@@ -0,0 +1,44 @@
+import tecplot as tp
+from tecplot.exception import *
+from tecplot.constant import *
+
+
+tp.session.connect(port=7600)
+# 定义目录
+distance_folder = ['48', '60', '72']
+nozzles_folder = ['5nozzle', '3nozzle', '1nozzle']
+scale_factor = [1, 0.9, 0.8, 0.7, 0.6, 0.45, 0.4, 0.35, 0.3, 0.25]
+quantities = ['velocity.lay', 'ch+.lay', 'flame.lay']
+for nozzles in nozzles_folder:
+    # E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\5nozzle
+    dir_nozzles = 'E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\postprocessing\\' + nozzles
+    print(dir_nozzles + ' is on processing')
+    for distance in distance_folder:
+        # E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\5nozzle\\48
+        dir_distance = dir_nozzles + '\\' + distance
+        for num in range(len(scale_factor)):
+            str_factors = str(scale_factor[num]) 
+            #  E:\\0-PhD\\5 nozzle\\5nozzle-DLN2.6SIZE\\5nozzle\\48\\48-1
+            dir_scale = dir_distance + '\\' + distance + '-' + str_factors
+            for quantity in quantities:
+                try:
+                    dir_tec = dir_scale + '\\' + quantity
+                    print(dir_tec + ' is on going')
+                    photo_name = quantity.replace('.lay','')
+                    dir_save = dir_scale + '\\' + photo_name + '.tiff'
+                    tp.macro.execute_command('$!RedrawAll')
+                    tp.load_layout(dir_tec)
+                    tp.export.save_tiff(dir_save,
+                        width=1642,
+                        region=ExportRegion.AllFrames,
+                        supersample=1,
+                        convert_to_256_colors=False,
+                        gray_scale_depth=None,
+                        byte_order=TIFFByteOrder.Intel)
+                    print(photo_name + ' photo exported')
+                except:
+                    print(dir_tec + ' error')
+                    continue
+input("all done")
+
+
