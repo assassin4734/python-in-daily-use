e88747263cb847a28e5aba6a3d23373d91c37f1d
assassin4734 <490645473@qq.com>
2022-11-21 16:20:11 +0800

    new changes

--- 0. python operate os/9. 重命名文件夹.py
+++ 0. python operate os/9. 重命名文件夹.py
@@ -1,9 +1,9 @@
 import os
 
 
-eq_folder = ["eq=0.55", "eq=0.65", "eq=0.75", "eq=0.85", "eq=0.95"]
+eq_folder = ["eq=0.55"]
 scale_factor = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-dir = r'C:\Users\Administrator\Desktop\postprocessing'
+dir = r'F:\eq\postprocessing'
 for eq in eq_folder:
     dir_eq = dir + "\\" + eq
     for scale in scale_factor:
